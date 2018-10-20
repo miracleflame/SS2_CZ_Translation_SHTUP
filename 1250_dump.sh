3abf61f161de287699c545330200350d3ef5c00a
miracleflame <miracle.flame@gmail.com>
2018-10-20 17:22:29 +0200

    Update 1250_dump.sh

--- 1250_dump.sh
+++ 1250_dump.sh
@@ -3,15 +3,15 @@
 SAVEIFS=$IFS
 IFS=$(echo -en "\n\b")
 
-for i in `find cestina ! -wholename "*/cl_dlls*" ! -wholename "*/resource*" ! -wholename "*/materials*" ! -wholename "*/models*" ! -wholename "*/vdata/hackterminals*" ! -path *.mp3* ! -path *.vcd* ! -path *.wav* ! -path strings.txt*`; do
+for i in `find UTF8 ! -path *.crf*`; do
 
 	if [ -d "$i" ]
 		then
-			echo "directory : ${i//cestina/cestina_1250}"
-			mkdir "${i//cestina/cestina_1250}" 2>/dev/null
+			echo "directory : ${i//UTF8/1250}"
+			mkdir "${i//UTF8/1250}" 2>/dev/null
 		else
 			echo "file : $i"
-		cstocs utf8 1250 "$i" > "${i//cestina/cestina_1250}"
+		cstocs utf8 1250 "$i" > "${i//UTF8/1250}"
 	fi	
 done
 
